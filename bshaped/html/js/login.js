599f7127a2d534506ef375e09a90d122c789844e
JungWooLee <wjddn3711@naver.com>
2021-11-03 19:35:41 +0900

    event css update

---
+++ bshaped/html/js/login.js
@@ -0,0 +1,11 @@
+const loginBtn = document.querySelector("#login-btn");
+const singUpBtn = document.querySelector("#signUp-btn");
+
+function loginBtnClick(event) {
+    event.preventDefault();
+    const id = document.querySelector("#loginId");
+    const pwd = document.querySelector("#loginPwd");
+}
+
+loginBtn.addEventListener("click", loginBtnClick);
+
